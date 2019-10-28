b431fe2c5f111864a2460caff8d460899db6af70
Allen Shin <geeuho@gmail.com>
2019-10-28 05:15:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,78 +1,115 @@
+def points_per_player(player, hashketball)
+  player.capitalize!
+  if hashketball[:home][:players].include?(player)
+   hashketball[:home][:players][player][:stats][:points]
+  elsif hashketball[:away][:players].include?(player)
+   hashketball[:away][:players][player][:stats][:points]
+  else
+   "No player found."
+  end
+end
 
-def num_points_scored(player_search)
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:player_name] == player_search
-        return player[:points]
-      end
+puts points_per_player("blanko", hashketball)
+
+#
+
+# Return the shoe size for any player:
+
+def player_shoe_size(player, hashketball)
+  player.capitalize!
+  if hashketball[:home][:players].include?(player)
+   hashketball[:home][:players][player][:shoe_size]
+  elsif hashketball[:away][:players].include?(player)
+   hashketball[:away][:players][player][:shoe_size]
+  else
+   "No player found."
+  end
+end
+
+puts player_shoe_size("taz", hashketball)
+
+
+#
+
+# Return both colors for any team:
+
+def team_colors(team, hashketball)
+  sym = team.to_sym
+  if hashketball.include?(sym)
+   hashketball[sym][:colors]
+  else
+   "Try home or away."
+  end
+end
+
+puts team_colors("away", hashketball)
+
+#
+
+# Return both teams names:
+
+def team_names(hashketball)
+  puts "The #{hashketball[:home][:team_name]} welcome the #{hashketball[:away][:team_name]}"
+  puts 
+end
+
+puts team_names(hashketball)
+
+#
+
+# Return all the player numbers for a team:
+
+def player_nums(team, hashketball)
+    sym = team.to_sym
+    player_numbers = []
+    
+      hashketball[sym][:players].each_value do |x|
+      player_numbers << x[:number]
+  end
+    player_numbers
+end
+
+puts player_nums("away", hashketball)
+
+
+# Return all the stats for a player:
+
+def player_stats(player_name, hashketball)
+  player_name.capitalize!
+  if hashketball[:home][:players].include?(player_name)
+    hashketball[:home][:players][player_name][:stats]
+  elsif hashketball[:away][:players].include?(player_name)
+    hashketball[:away][:players][player_name][:stats]
+  else
+    "No player found."
+  end
+end
+
+puts player_stats("Daffy", hashketball)
+
+#
+
+# Return the rebounds for the player with the largest shoe size
+
+def biggest_shoe(hashketball)
+  player_number_with_biggest_shoe = ""
+  biggest_shoe_size = 0
+  
+  hashketball[:home][:players].each do |name, player|
+    if player[:shoe_size] > biggest_shoe_size
+      player_number_with_biggest_shoe = name
+      biggest_shoe_size = player[:shoe_size]
     end
   end
-end
-
-def shoe_size(name)
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:player_name] == name
-        return player[:shoe]
-      end
+  hashketball[:away][:players].each do |name, player|
+    if player[:shoe_size] > biggest_shoe_size
+      player_number_with_biggest_shoe = name
+      biggest_shoe_size = player[:shoe_size]
     end
   end
+  
+  " #{player_number_with_biggest_shoe} : #{biggest_shoe_size} "
 end
 
-def team_colors(team_input)
-  if team_input.downcase == "charlotte hornets" 
-    return game_hash[:away][:colors]
-  else return game_hash[:home][:colors]
-  end
-end
-
-def team_names
-  game_hash.map do |team, team_info|
-    team_info[:team_name]
-  end
-end
-
-def player_numbers(input)
-  output = []
-  game_hash.each do |team, team_info|
-    if team_info[:team_name] == input 
-      team_info.each do |key, value|
-        if key == :players
-          value.each do |player|
-          output.push(player[:number])
-          end
-        end
-      end
-    end
-  end
-  return output
-end
-
-def player_stats(input)
-  game_hash.each do |team, team_info|
-    team_info.each do |key, value|
-      if key == :players
-        value.each do |player|
-          if input == player[:player_name]
-            player.delete(:player_name) # having player name inside the hash was a bad idea!
-            return player
-          end
-        end
-      end
-    end
-  end
-end
-
-def big_shoe_rebounds
-  big_shoe = 0
-  rebounds = 0
-  game_hash.each do |team, team_info|
-    team_info[:players].each do |player|
-      if player[:shoe] > big_shoe
-        big_shoe = player[:shoe]
-        rebounds = player[:rebounds]
-      end
-    end
-  end
-  return rebounds
-end
+puts biggest_shoe(hashketball)
+  
